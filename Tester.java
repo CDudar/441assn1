db1e92d218c775f9395953c90d90962b9e97e1b5
Christian Dudar <CDudar@users.noreply.github.com>
2017-10-04 15:21:36 -0600

    These files work on Windows

--- Tester.java
+++ Tester.java
@@ -16,10 +16,10 @@ public class Tester {
 
 		// include whatever URL you like
 		// these are just some samples
-		String[] url = {//"people.ucalgary.ca/~mghaderi/index.html",
-						//people.ucalgary.ca/~mghaderi/test/uc.gif"
-						"people.ucalgary.ca/~mghaderi/test/a.pdf"
-						//"people.ucalgary.ca:80/~mghaderi/test/test.html"
+		String[] url = {"people.ucalgary.ca/~mghaderi/index.html",
+						"people.ucalgary.ca/~mghaderi/test/uc.gif",
+						"people.ucalgary.ca/~mghaderi/test/a.pdf",
+						"people.ucalgary.ca:80/~mghaderi/test/test.html"
 				};
 		
 		// this is a very basic tester
